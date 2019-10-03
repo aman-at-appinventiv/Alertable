1ef4359956ea00690c1d60042875bf5f4375fd8d
Aman Chawla <aman.chawla@appinventiv.com>
2019-10-03 13:23:54 +0530

    Initial Commit

---
+++ Sources/Alertable/Alertable.swift
@@ -0,0 +1,29 @@
+//
+//  Alertable.swift
+//  SPMTest
+//
+//  Created by Appinventiv on 03/10/19.
+//  Copyright © 2019 Appinventiv. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public protocol Alertable { }
+
+extension Alertable where Self: UIViewController {
+    
+    public func showAlert(withTitle title: String, andMsg msg: String) {
+        let alertController = UIAlertController(title: title, message: msg, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    public func showActionSheet(withTitle title: String, andMsg msg: String) {
+        let alertController = UIAlertController(title: title, message: msg, preferredStyle: .actionSheet)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
+}
