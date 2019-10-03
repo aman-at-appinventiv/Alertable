0eb4b6792c21f51dfd25a3c7619f7017276e5ccc
Aman Chawla <aman.chawla@appinventiv.com>
2019-10-03 15:52:18 +0530

    remove public function

--- Sources/Alertable/Alertable.swift
+++ Sources/Alertable/Alertable.swift
@@ -13,21 +13,21 @@ public protocol Alertable { }
 
 extension Alertable where Self: UIViewController {
     
-    public func showAlert(withTitle title: String, andMsg msg: String) {
+    func showAlert(withTitle title: String, andMsg msg: String) {
         let alertController = UIAlertController(title: title, message: msg, preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
         alertController.addAction(okAction)
         present(alertController, animated: true, completion: nil)
     }
     
-    public func showActionSheet(withTitle title: String, andMsg msg: String) {
+    func showActionSheet(withTitle title: String, andMsg msg: String) {
         let alertController = UIAlertController(title: title, message: msg, preferredStyle: .actionSheet)
         let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
         alertController.addAction(okAction)
         present(alertController, animated: true, completion: nil)
     }
     
-    public func showCancelAlert(withTitle title: String, andMsg msg: String) {
+    func showCancelAlert(withTitle title: String, andMsg msg: String) {
         let alertController = UIAlertController(title: title, message: msg, preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
